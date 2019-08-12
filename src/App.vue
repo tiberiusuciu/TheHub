74b8308055cdd7ae801ec5d19478af633ab82c0b
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2019-08-12 18:05:02 -0400

    Added the central hub

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,227 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="name-area">
+      <h1>Tiberiu Cristian Suciu</h1>
+      <div class="line"></div>
+    </div>
+    <div class="project-list">
+      <template v-for="(project, index) in projects">
+        <ProjectItem :info="project" :key="index" :colors="selectedColors" />
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProjectItem from './components/ProjectItem.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    ProjectItem
+  },
+  created() {
+    this.selectedColors = this.colors[Math.floor(Math.random()*this.colors.length)];
+    this.projects.sort(function(a, b) {
+        var textA = a.title.toUpperCase();
+        var textB = b.title.toUpperCase();
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+  },
+  data() {
+    return {
+      selectedColors: '',
+      projects: [
+        {
+          title: 'B21',
+          description: 'A multiplayer blackjack application made with VueJS',
+          link: '',
+          sourceLink: '',
+          isReady: false
+        },
+        {
+          title: 'Songify',
+          description: 'An application to convert videos into mp3 files',
+          link: '',
+          sourceLink: '',
+          isReady: false
+        },
+        {
+          title: 'Resume',
+          description: 'My online resume',
+          link: '',
+          sourceLink: '',
+          isReady: false
+        },
+        {
+          title: 'Blog',
+          description: 'A tailor made application for my blog',
+          link: '',
+          sourceLink: '',
+          isReady: false
+        },
+        {
+          title: 'MTGVue',
+          description: 'A Magic the Gathering application that lets you play with others',
+          link: '',
+          sourceLink: 'https://github.com/tiberiusuciu/mtgvue',
+          isReady: false
+        },
+        {
+          title: 'Business',
+          description: 'An application for anyone to get in contact with me so I can build them websites and applications',
+          link: '',
+          sourceLink: 'https://github.com/tiberiusuciu/Portfolio',
+          isReady: false
+        },
+        {
+          title: 'GameJams',
+          description: 'An application that groups all my game jam entries',
+          link: '',
+          sourceLink: '',
+          isReady: false
+        },
+        {
+          title: 'QList',
+          description: 'Task Management tool',
+          link: '',
+          sourceLink: 'https://github.com/tiberiusuciu/QList',
+          isReady: false
+        },
+        {
+          title: 'Financial',
+          description: 'Personal finance management tool',
+          link: '',
+          sourceLink: 'https://github.com/tiberiusuciu/financial',
+          isReady: false
+        },
+        {
+          title: 'Foodies',
+          description: 'Recipe Manager tool',
+          link: '',
+          sourceLink: 'https://github.com/tiberiusuciu/foodies',
+          isReady: false
+        },
+        {
+          title: 'Chopify',
+          description: 'Audio track editor',
+          link: '',
+          sourceLink: '',
+          isReady: false
+        },
+        {
+          title: 'ETS',
+          description: 'School application to seek help and offer help',
+          link: '',
+          sourceLink: '',
+          isReady: false
+        },
+        {
+          title: 'rpg-io',
+          description: 'Terminal-based MMORPG',
+          link: '',
+          sourceLink: '',
+          isReady: false
+        },
+      ],
+      colors: [
+        {
+          primary: 'rgba(76,161,175,1)',
+          secondary: 'rgba(196,224,229,1)'
+        },
+        {
+          primary: 'rgba(0,82,212,1)',
+          secondary: 'rgba(111,177,252,1)'
+        },
+        {
+          primary: 'rgba(71,118,230,1)',
+          secondary: 'rgba(142,84,233,1)'
+        },
+        {
+          primary: 'rgba(255,81,47,1)',
+          secondary: 'rgba(221,36,118,1)'
+        }
+      ]
+    }
+  },
 }
 </script>
 
 <style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
+body {
+  background-color: ghostwhite;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: black;
+  display: flex;
+  height: 100vh;
+}
+
+.name-area {
+  box-sizing: border-box;
+  flex: 1.5;
+  padding-top: 375px;
+  padding-left: 200px;
+  border-right: 1px solid black;
+}
+
+.project-list {
+  flex: 1;
+  overflow-y: auto;
+  /* background-color: #e6e6e6; */
+}
+
+.name-area h1 {
+  letter-spacing: 3px;
+}
+
+.name-area .line {
+  background-color: black;
+  width: 240px;
+  height: 5px;
+  margin-top: 10px;
+  margin-left: 10px;
+}
+
+@media screen and (max-width: 800px) {
+    #app {
+      display: flex;
+      flex-direction: column;
+    }
+    .project-list, .name-area {
+      flex: none;
+      padding: 0px;
+    }
+
+    .name-area {
+      height: 200px;
+      padding-top: 75px;
+      padding-left: 10px;
+      border-right: none;
+      border-bottom: 1px solid black;
+    }
+
+    .project-list {
+      flex: 1;
+    }
+}
+
+@media screen and (min-width: 800px) and (max-height: 700px) {
+  .name-area {
+    box-sizing: border-box;
+    flex: 1;
+    padding-top: 150px;
+    padding-left: 10px;
+    /* border-right: 1px solid black; */
+  }
 }
 </style>
---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="project">
+        <div class="title-area">
+            <div class="project-title">
+                <h2>{{info.title}}</h2>
+            </div>
+            <template v-if="info.isReady">
+                <a class="app-link" :href="info.link" target="_blank">
+                    <div class="goto-link" :style="{background: colors.primary, background: 'linear-gradient(90deg, ' + colors.primary + '0%, ' + colors.secondary + ' 100%)'}">
+                        Visit
+                    </div>
+                </a>
+            </template>
+            <template v-else>
+                <div class="goto-link in-construction">
+                    <i class="fad fa-construction"></i>
+                </div>
+            </template>
+        </div>
+        <div class="description-area">
+            <div>
+                {{info.description}}
+            </div>
+            <div v-if="info.sourceLink !== ''">
+                <a class="sourcelink" :href="info.sourceLink" target="_blank" :style="{color: colors.primary}">
+                    (see source <i class="fab fa-github"></i>)
+                </a>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectItem',
+  props: ['info', 'colors']
+}
+</script>
+
+<style scoped>
+    .project {
+        /* background-color: #f9f; */
+        padding: 20px 20px;
+        /* height: 60px; */
+        /* border: 1px solid black; */
+        /* border-bottom: 1px solid black; */
+    }
+
+    .title-area {
+        display: flex;
+    }
+
+    .project-title {
+        flex: 1;
+        padding: 4px 0px;
+    }
+
+    .goto-link {
+        width: 100px;
+        line-height: 38px;
+        /* background: rgb(208,145,209);
+        background: linear-gradient(90deg, rgba(208,145,209,1) 0%, rgba(94,92,232,1) 100%); */
+        text-align: center;
+        font-weight: bold;
+        color: white;
+        border-radius: 4px;
+        cursor: pointer;
+        letter-spacing: 1px;
+    }
+
+    .description-area {
+        margin-top: 10px;
+    }
+
+    .in-construction {
+        color: black;
+        font-size: 1.5em;
+        color: orange;
+        --fa-secondary-color: orange;
+        --fa-primary-color: black;
+        --fa-secondary-opacity: 1.0;
+    }
+    
+    .app-link {
+        text-decoration: none;
+    }
+</style>
